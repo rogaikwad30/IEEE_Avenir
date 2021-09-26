207c8543038465ef9de4a443edc01cbf4c814a7b
Atharva Deore <www.atharvadeore22@gmail.com>
2021-09-26 21:20:26 +0530

    pre

--- public/js/colonise.js
+++ public/js/colonise.js
@@ -1,65 +1,47 @@
- 
-let container;
-let camera;
-let renderer;
-let scene;
-let house;
 
-function init() {
-  container = document.querySelector(".scene");
 
-  //Create scene
+let container , camera , house, scene ;
+
+function init() { 
+  container = document.querySelector(".scene"); 
   scene = new THREE.Scene();
-
-  const fov = 35;
-  const aspect = container.clientWidth / container.clientHeight;
-  const near = 0.1;
-  const far = 1000;
-
-  //Camera setup
+  const fov = 35, aspect = container.clientWidth / container.clientHeight, near = 0.1, far = 1000;
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 5, 20);
-
+  camera.position.set(25, 10, -375);
   const ambient = new THREE.AmbientLight(0xD3FAD6, 2);
   scene.add(ambient);
-
   const light = new THREE.DirectionalLight(0xEDEBA0, 2);
   light.position.set(0,0,0);
   scene.add(light);
-  //Renderer
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(container.clientWidth, container.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
-  let controls = new THREE.OrbitControls(camera , renderer.domElement );
-  controls.addEventListener('change', renderer);
-  container.appendChild(renderer.domElement);
- 
-  let directionalLight = new THREE.DirectionalLight(0x8E3B46,100);
-  directionalLight.position.set(00, 00, 1);
-  directionalLight.castShadow = true;
-  scene.add(directionalLight); 
 
+   let controls = new THREE.OrbitControls(camera , renderer.domElement );
+   controls.addEventListener('change', renderer);
+
+  container.appendChild(renderer.domElement);
+  // const light = new THREE.DirectionalLight(0xEDEBA0, 2);
+  // light.position.set(0,0,0);
+  // scene.add(light);
   let loader = new THREE.GLTFLoader();
-  loader.load("../js/3dIcons/seven/scene.gltf", function(gltf) {
+  loader.load("../js/3dIcons/nine/scene.gltf", function(gltf) {
     scene.add(gltf.scene);
-    house = gltf.scene.children[0];
-    house.scale.set(1,0.95,1)
-    house.position.set(0,0,0)
+    house = gltf.scene.children[0]; 
+    house.scale.set(5,5,5)
     animate();
   });
 }
-function animate() {
+function animate( ) {
   requestAnimationFrame(animate);
-  house.rotation.z += 0.001;
+  house.rotation.z += 0.002 
   renderer.render(scene, camera);
 }
-
 init();
 
 function onWindowResize() {
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
--- public/js/r_and_d.js
+++ public/js/r_and_d.js
@@ -1,45 +1,68 @@
-let container , camera , house, scene ;
+ 
+let container;
+let camera;
+let renderer;
+let scene;
+let house;
 
-function init() { 
-  container = document.querySelector(".scene"); 
+function init() {
+  container = document.querySelector(".scene");
+
+  //Create scene
   scene = new THREE.Scene();
-  const fov = 35, aspect = container.clientWidth / container.clientHeight, near = 0.1, far = 1000;
+
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+
+  //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(25, 10, -375);
+  camera.position.set(0, 5, 20);
+
   const ambient = new THREE.AmbientLight(0xD3FAD6, 2);
   scene.add(ambient);
+
   const light = new THREE.DirectionalLight(0xEDEBA0, 2);
   light.position.set(0,0,0);
   scene.add(light);
+  //Renderer
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(container.clientWidth, container.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
-
-   let controls = new THREE.OrbitControls(camera , renderer.domElement );
-   controls.addEventListener('change', renderer);
-
+  let controls = new THREE.OrbitControls(camera , renderer.domElement );
+  controls.addEventListener('change', renderer);
   container.appendChild(renderer.domElement);
-  
+ 
+  let directionalLight = new THREE.DirectionalLight(0x8E3B46,100);
+  directionalLight.position.set(00, 00, 1);
+  directionalLight.castShadow = true;
+  scene.add(directionalLight); 
+
   let loader = new THREE.GLTFLoader();
-  loader.load("../js/3dIcons/eight/scene.gltf", function(gltf) {
+  loader.load("../js/3dIcons/seven/scene.gltf", function(gltf) {
     scene.add(gltf.scene);
-    house = gltf.scene.children[0]; 
-    house.position.set(0,-100,0)
-    house.scale.set(0.05,0.066,0.067)
+    house = gltf.scene.children[0];
+    house.scale.set(1,0.95,1)
+    house.position.set(0,0,0)
     animate();
   });
 }
-function animate( ) {
+function animate() {
   requestAnimationFrame(animate);
-  house.rotation.z += 0.002 
+  house.rotation.z += 0.001;
   renderer.render(scene, camera);
 }
+
 init();
 
 function onWindowResize() {
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
+
   renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
 window.addEventListener("resize", onWindowResize);
+
+
